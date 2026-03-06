592e64f6129e6e9f85cd473b4d85747a76bd228d
Alkazuz <kerallis2015@gmail.com>
2026-03-06 13:52:06 -0300

    refactor: improve comments for clarity in buildApp function

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,18 @@ import Fastify from 'fastify';
 import { setErrorHandlers } from './errors/error-handler.js';
 import { registerRoutes } from './routes/index.js';
 
+// Função para construir a aplicação Fastify
 export function buildApp() {
+  // Criar uma instância do Fastify com o logger habilitado
   const app = Fastify({
     logger: true,
   });
 
+  // Configurar os manipuladores de erros
   setErrorHandlers(app);
+  // Registrar as rotas
   void app.register(registerRoutes);
 
+  // Retornar a instância do aplicativo Fastify
   return app;
 }
